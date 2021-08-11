63d9ecc15390f92a38523544e77b62d0f104e7ee
Your Name <you@example.com>
2021-08-11 16:00:12 +0000

    1st task

--- vampire.js
+++ vampire.js
@@ -1,5 +1,5 @@
 class Vampire {
-  constructor(name, yearConverted) {
+  constructor(name = 'vamp', yearConverted = 1800 ) {
     this.name = name;
     this.yearConverted = yearConverted;
     this.offspring = [];
@@ -10,22 +10,34 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire); 
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfPeople = 0;
+    let currentVamp = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVamp.creator) {
+      currentVamp  = currentVamp.creator;
+      numberOfPeople++;
+    }
+
+    return numberOfPeople;
   }
+
+  
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
